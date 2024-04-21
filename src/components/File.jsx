eda67e0acf9fe9175cc242289455202c54597128
Maksim-1307 <max.07mal@gmail.com>
2024-04-21 21:17:54 +0300

    download link and error handing

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -6,27 +6,51 @@ import { useLocation } from "react-router-dom";
 
 function File() {
 
-    const noteContent = (<>
-        <p>В тексте обнаружено <b>17</b> упоминаний <b>4</b> иноагентов или нежелательных организаций. </p>
-        <p>Места в тексте с выявленными совпадениямиотмечены цветом. Мы не храним ваши загруженные документы и сформированные отчеты, но вы можете скачать отчет о сверке этого текста в формате pdf. </p>
-        <a class="btn-1" href="#">Скачать отчет</a>
-    </>);
+    try{
+        const location = useLocation();
 
-    const location = useLocation();
+        function countStats() {
+            let total = location["state"]["found"].length;
+            let count = 0;
+            location["state"]["found"].forEach(element => {
+                count += element.count;
+            });
+            return {
+                total: total,
+                count: count
+            }
+        }
 
-    return (
-        <section class="file-body">
-            <div class="container file-body__container">
-                <div class="file-body__left">
-                    <FileContent content={location["state"]["content"]}/>
+        const stats = countStats();
+
+        const noteContent = (<>
+            <p>В тексте обнаружено <b>{stats.total}</b> упоминаний <b>{stats.count}</b> иноагентов или нежелательных организаций. </p>
+            <p>Места в тексте с выявленными совпадениямиотмечены цветом. Мы не храним ваши загруженные документы и сформированные отчеты, но вы можете скачать отчет о сверке этого текста в формате pdf. </p>
+            <a class="btn-1" href={location["state"]["download_link"]} download>Скачать отчет</a>
+        </>);
+
+        return (
+            <section class="file-body">
+                <div class="container file-body__container">
+                    <div class="file-body__left">
+                        <FileContent content={location["state"]["content"]} />
+                    </div>
+                    <div class="file-body__right">
+                        <Note content={noteContent} />
+                        <Report content={location["state"]["found"]} />
+                    </div>
                 </div>
-                <div class="file-body__right">
-                    <Note content={noteContent}/>
-                    <Report content={location["state"]["found"]}/>
-                </div>
+            </section>
+        );
+    } catch {
+        return (
+        <section>
+            <div class="container">
+                <div>Ошибка загрузки контента</div><br/>
+                <a class="btn-1" href="..">вернуться на главную</a>
             </div>
-        </section>
-    );
+        </section>);
+    }
 }
 
 export default File;
