f268bc6217b59f9f9af2c527c3c268ee39188111
Maksim-1307 <max.07mal@gmail.com>
2024-04-22 08:53:06 +0300

    drag&drop

--- src/components/UploadField.jsx
+++ src/components/UploadField.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import UploadButton from './UploadButton';
 import fileSubmit from '../helpers/FileUploader.js'
+import { useRef } from 'react';
 
 import plusIcon from '../img/icons/plus-icon.svg'
 import preloaderIcon from '../img/icons/preloader.svg'
@@ -11,6 +12,7 @@ function UploadField(){
     let [isWaiting, setWaiting] = useState(false);
     const [file, setFile] = useState(null);
     const navigate = useNavigate();
+    const myRef = useRef(null);
 
     const defaultText = "Перетащите в эту область файл для пооверки (doc, docx или pdf до 10 Мб) или нажмите на кнопку";
     const loadingText = "Документ обрабатывается. Это может занять некоторое время, не закрывайте вкладку браузера";
@@ -35,6 +37,26 @@ function UploadField(){
         return;
     }
 
+    const handleDrop = function (e) {
+        e.preventDefault();
+        console.log('file');
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            setFile(e.dataTransfer.files[0]);
+        }
+    };
+
+    const handleDragOverLeave = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    // $(".upload-field").on("dragover", );
+
+    // $(".upload-field").on("dragleave", function (event) {
+    //     event.preventDefault();
+    //     event.stopPropagation();
+    // });
+
     useEffect(() => {
         if (file) {
             fileSubmit(file, 'https://info-marker.ru/api/files/upload.php', processResponse);
@@ -51,7 +73,7 @@ function UploadField(){
     return (
         <section>
           <div class="container">
-            <div class="upload-field nice-border">
+                <div class="upload-field nice-border" ref={myRef} onDrop={handleDrop} onDragOver={handleDragOverLeave} onDragLeave={handleDragOverLeave}>
                 <img class="upload-field__icon" src={isWaiting ? preloaderIcon : plusIcon}/>
                 <p class="upload-field__text">{isWaiting ? loadingText : defaultText}</p>
                 {isWaiting ? null : <UploadButton data={data} /> }
