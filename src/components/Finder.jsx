76fe4f1d7f1e246a8fcf34e89ad5c50cbee95456
Maksim-1307 <max.07mal@gmail.com>
2024-05-08 23:51:19 +0300

    bug fixes

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -13,8 +13,6 @@ function Finder (props) {
     const [id, setId] = useState(0);
     const [color, setColor] = useState();
     const [count, setCount] = useState(0);
-    const [distanceFromTop, setDistanceFromTop] = useState(1000);
-    const [shouldFixed, setShouldFixed] = useState(false);
     const { selectedName, setName} = useContext(fileContext);
     const finderRef = useRef(null)
     const fillerRef = useRef(null)
@@ -57,20 +55,10 @@ function Finder (props) {
         setId(newId);
     }
 
-    useEffect(()=>{
-        if (selectedName && selectedName in props.data) {
-            setColor(props.data[selectedName].color);
-            setCount(props.data[selectedName].count);
-        } 
-        elements = get_elements();
-        
-    }, [selectedName]);
-
-
-    useEffect(() => {
+    function focus(){
 
         if (!elements[id]) return;
-        
+
         elements[id].scrollIntoView({
             behavior: 'smooth',
             block: 'center',
@@ -81,7 +69,7 @@ function Finder (props) {
                 position: 'relative',
                 top: '0'
             },
-            { 
+            {
                 position: 'relative',
                 top: '-5px'
             },
@@ -101,86 +89,51 @@ function Finder (props) {
         {
             duration: 1000,
         });
+    }
+
+    useEffect(()=>{
+        if (selectedName && selectedName in props.data) {
+            setColor(props.data[selectedName].color);
+            setCount(props.data[selectedName].count);
+        } 
+        elements = get_elements();
+
+        move(-1);
+        
+    }, [selectedName]);
+
+
+    useEffect(() => {
+        focus();
     }, [id]);
 
-    // useEffect(() => { 
-    //     console.log("effect");
-    //     if (finderRef.current && fillerRef.current){
-    //         const height = parseInt(finderRef.current.offsetHeight);
-    //         const spaceFiller = fillerRef.current;
-    //         spaceFiller.style.height = height + "px";
-    //         console.log("height changed");
-    //     } else {
-    //         if (finderRef.current){
-    //             console.log("finder ref only");
-    //         }
-    //     }
-    // });
-
-    // useEffect(() => {
-    //     const observer = new IntersectionObserver((entries) => {
-    //         entries.forEach((entry) => {
-    //             const distance = entry.boundingClientRect.top;
-    //             if (distance <= 0){
-    //                 setShouldFixed(true);
-    //             } else {
-    //                 setShouldFixed(false);
-    //             }
-    //             setDistanceFromTop(distance);
-    //         });
-    //     }, 
-    //     {
-    //         rootMargin: '-10px 0px 0px 0px' 
-    //     }
-    // );
- 
-    //     if (fillerRef.current) {
-    //         observer.observe(fillerRef.current);
-    //         return () => {
-    //             observer.unobserve(fillerRef.current);
-    //         };
-    //     }
-
-    // });
-
+    
     window.onscroll = () => {
         const finder = document.getElementsByClassName("finder")[0];
         const spaceFiller = document.getElementsByClassName("finder__space-filler")[0]; 
         if (finder && spaceFiller){ 
             const topRect = parseInt(spaceFiller.getBoundingClientRect().top);
             const wasFixed = parseFloat(finder.getAttribute("finder-is-fixed"));
-            console.log(topRect);
-            if (topRect >= 0) {
+            if (topRect >= 20) {
                 if (wasFixed) {
-                    finder.style.position = "relative";
+                    finder.style.position = "static";
                     finder.style.width = "100%";
+                    spaceFiller.style.height = "0px";
                 }
                 finder.setAttribute("finder-is-fixed", "0");
             } else {
                 if (!wasFixed) {
                     const width = finder.offsetWidth;
+                    const height = finder.offsetHeight;
                     finder.style.maxWidth = width + "px";
                     finder.style.position = "fixed";
+                    spaceFiller.style.height = height + "px";
+                    console.log(height);
                 }
                 finder.setAttribute("finder-is-fixed", "1");
             }
         } 
     }
-
-    useEffect(()=>{
-        const finder = finderRef.current;
-        const spaceFiller = fillerRef.current;
-        if (finder && spaceFiller) {
-            if (shouldFixed) {
-                finder.style.position = "fixed";
-                const height = parseInt(finder.offsetHeight); 
-                spaceFiller.style.height = height + "px";
-            } else {
-                finder.style.position = "relative";
-                spaceFiller.style.height = 0 + "px";
-            }
-        }
-    }, [shouldFixed]);
 
     let counter = {
         id: id,
@@ -190,7 +143,8 @@ function Finder (props) {
 
     if (selectedName) {
         return (
-            <>
+            <div className="finder__wrapper">
+                <div ref={fillerRef} className="finder__space-filler"></div>
                 <div ref={finderRef} className="finder" finder-is-fixed="0" style={{ "background-color": "#" + color }}>
                     <div className="finder__text">{selectedName}</div>
                     <div className="finder__bottom">
@@ -199,8 +153,7 @@ function Finder (props) {
                         <button className="finder__next" onClick={() => { move(1) }}><img src={arrow} alt="" /></button>
                     </div>
                 </div>
-                <div ref={fillerRef} className="finder__space-filler"></div>
-            </>
+            </div>
         );
     } 
 
@@ -208,42 +161,3 @@ function Finder (props) {
 
 export default Finder;
 
-
-
-/*
-
-
-import React, { useEffect, useRef, useState } from 'react';
-
-const ComponentWithScrollTracking = () => {
-  const componentRef = useRef(null);
-  const [distanceFromTop, setDistanceFromTop] = useState(0);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => { 
-      entries.forEach((entry) => {
-        const distance = entry.boundingClientRect.y;
-        setDistanceFromTop(distance);
-      });
-    }, {
-      rootMargin: '-10px 0px 0px 0px' // отступ сверху: -10px
-    });
-
-    observer.observe(componentRef.current);
-
-    return () => {
-      observer.unobserve(componentRef.current);
-    };
-  }, []);
-
-  return (
-    <div ref={componentRef} style={{ marginTop: '50px' }}>
-      <p>Distance from top: {distanceFromTop}px</p>
-    </div>
-  );
-};
-
-export default ComponentWithScrollTracking;
-
-
-*/
